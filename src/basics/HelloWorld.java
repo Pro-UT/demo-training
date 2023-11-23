d5ba809a9c1963dfced9fd909ee0597c0c117ca6
Pro-UT <68873282+Pro-UT@users.noreply.github.com>
2023-11-23 17:23:22 +0530

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
  public static void main(String[] args) {
 	 System.out.println("hello world");
 	 System.out.println("i'm adding this code to the project");
+	 System.out.println("i'm adding this code.");
  }
 }
