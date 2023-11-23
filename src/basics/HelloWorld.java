49ed8059115259b1e82068f79192ad68223202b8
Utkarsh Tiwari <utkarshs777@gmail.com>
2023-11-23 17:11:29 +0530

    Modified Hello World app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,5 +3,6 @@ package basics;
 public class HelloWorld {
  public static void main(String[] args) {
 	 System.out.println("hello world");
+	 System.out.println("i'm adding this code to the project");
  }
 }
